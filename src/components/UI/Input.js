0ae6d7dfeeefd4aa81d9b06bf9e133324e1e3433
oguz gül <ogzgl95@outlook.com>
2021-07-11 00:18:57 +0300

    Modified MealItem's of the components and CartProvider and Input

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,13 @@
 import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((prop, ref) => {
   return (
     <div className={classes.input}>
-      <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <label htmlFor={prop.input.id}>{prop.label}</label>
+      <input ref={ref} {...prop.input} />
     </div>
   );
-};
+});
 
 export default Input;
